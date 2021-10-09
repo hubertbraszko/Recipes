023f5d5f2d8c0a2bf081b23a8343d710f4ead8ef
hubertbraszko <hubert.braszko@gmail.com>
2021-10-09 16:11:59 +0200

    security adjustments

--- Recipes/task/src/Security/WebSecurityConfigurerImpl.java
+++ Recipes/task/src/Security/WebSecurityConfigurerImpl.java
@@ -3,6 +3,7 @@ package Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,7 +18,7 @@ public class WebSecurityConfigurerImpl extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password(getEncoder().encode("admin"))
-                .roles("admin")
+                .roles("ADMIN")
                 .and()
                 .passwordEncoder(getEncoder());
 
@@ -25,11 +26,11 @@ public class WebSecurityConfigurerImpl extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .mvcMatchers("/**").hasRole("ADMIN")
-                .mvcMatchers("/api/register").permitAll()
-                .and().httpBasic().and().formLogin();
+        http.authorizeRequests().antMatchers("/h2/**").permitAll().antMatchers("/**").permitAll();
+                http.csrf().disable().headers().frameOptions().disable();
     }
+
+
 
     @Bean
     public PasswordEncoder getEncoder() {
